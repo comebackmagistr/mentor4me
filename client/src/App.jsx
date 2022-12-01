e78009a32b1e4a25e40f81ff02e62fe4a7b3f644
Sofia Shidakova <fshidakova@mail.ru>
2022-12-01 18:44:46 +0300

    cropphoto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import DownLoad from './components/DownLoad';
 import MainPage from './components/MainPage';
 import NavBar from './components/NavBar';
+// import FileInput from './components/FileInput';
+import Crop from './components/Crop';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route element={<MainPage />} path="/" />
+        <Route element={<DownLoad />} path="/down" />
+        <Route element={<Crop />} path="/input" />
+
       </Routes>
     </>
   );

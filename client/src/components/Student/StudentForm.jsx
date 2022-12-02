1710824d34e96d17a7d296e6c8c1d87ae5110b79
Sesegma <anveegres@icloud.com>
2022-12-02 12:23:46 +0300

    cabinetday2

--- client/src/components/Student/StudentForm.jsx
+++ client/src/components/Student/StudentForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -6,24 +6,39 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useDispatch, useSelector } from 'react-redux';
-import { showStudent } from '../../redux/studentSlice';
+import { showStudent, editStudent } from '../../redux/slices/studentSlice';
 
 export default function Profile() {
   const student = useSelector((store) => store.student);
+  const [isTrue, setIsTrue] = useState(true);// для кнопки редактирования
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(showStudent());
   }, []);
+  const editHandler = () => {
+    setIsTrue((prev) => !prev);
+    dispatch(editStudent(student));
+  };
   return (
     <Card sx={{ maxWidth: 900 }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {student.firstName}
-          {' '}
-          {student.lastName}
+          {isTrue === true ? (
+            <>
+              {student?.firstName}
+              {' '}
+              {student?.lastName}
+            </>
+          ) : (
+            <>
+              <input name="firstName" type="text" placeholder={student?.firstName} />
+              <input name="lastName" type="text" placeholder={student?.lastName} />
+            </>
+          )}
         </Typography>
         <CardActions>
-          <Button size="small">Редактировать</Button>
+          <Button onClick={() => setIsTrue((prev) => !prev)} size="small">Редактировать</Button>
+          {!isTrue && <Button onClick={() => editHandler()} size="small">Сохранить</Button> }
         </CardActions>
         <CardMedia
           component="img"
@@ -35,26 +50,37 @@ export default function Profile() {
         <Typography variant="body2" color="text.secondary">
           email:
           {' '}
-          {student.email}
+          {isTrue === true ? (
+            student?.email
+          ) : (
+            <input name="email" type="text" placeholder={student?.email} />
+          )}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           zoom:
           {' '}
-          {student.zoom}
+          {isTrue === true ? (
+            student?.zoom
+          ) : (
+            <input name="zoom" type="text" placeholder={student?.zoom} />
+          )}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           phone:
           {' '}
-          {student.phone}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          price:
-          {' '}
-          {student.price}
+          {isTrue === true ? (
+            student?.phone
+          ) : (
+            <input name="phone" type="text" placeholder={student?.phone} />
+          )}
         </Typography>
         <h5>Профессиональная сфера:</h5>
         <Typography variant="body2" color="text.secondary">
-          врапвророаы
+          {isTrue === true ? (
+            student?.profScill // в бд надо ли добавить профориент?
+          ) : (
+            <input name="profScill" type="text" placeholder={student?.profScill} />
+          )}
         </Typography>
         <CardActions>
           <Button size="small">Записаться на занятие</Button>

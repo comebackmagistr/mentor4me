bd36af87be5467a8b80d01ae1bd3d95dc87ea141
Sofia Shidakova <fshidakova@mail.ru>
2022-12-02 15:20:51 +0300

    fix crop

---
+++ server/routes/cropMentorPhotoRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { Mentor } = require('../db/models');
+const file = require('../middleware/file');
+
+const router = express.Router();
+router.post('/', file.single('crop'), async (req, res) => {
+  try {
+    if (req.file) {
+      console.log(req.file);
+      console.log(req.file.filename);
+      const fileName = req.file.filename;
+      await Mentor.create({ photo: fileName });
+      // await Mentor.create({ photo: filePath.substring(7) });
+
+      res.json(req.file);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+module.exports = router;

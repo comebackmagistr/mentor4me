3ba772b4f009810e65cd561c2e71338a8222d4f6
Sesegma <anveegres@icloud.com>
2022-12-03 17:30:29 +0300

    newmentorstudent

--- server/routes/studentRouter.js
+++ server/routes/studentRouter.js
@@ -5,20 +5,34 @@ const router = express.Router();
 
 // show student
 router.get('/studentprofile', async (req, res) => {
+  const { id } = req.session.user.id;
   try {
-    const student = await Student.findOne({ where: { firstName: 'Shkolnik' } }); // здесь нужно исправить на req.session.user.id
+    const student = await Student.findOne({ where: { id } });
     res.json(student);
   } catch (error) {
     console.log(error);
   }
 });
 
-// router.patch('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const { text } = req.body; // что будет здесь?
-//   await Student.update({ text }, { where: { id } }); // что будет здесь?
-//   const newStudent = await Student.findByPk(id); // что будет здесь?
-//   res.json(newStudent);
-// });
+// edit student
+router.patch('/studentprofile', async (req, res) => {
+  const { id } = req.session.user.id;
+  const {
+    firstName,
+    lastName,
+    email,
+    zoom,
+    phone,
+  } = req.body;
+  await Student.update({
+    firstName,
+    lastName,
+    email,
+    zoom,
+    phone,
+  }, { where: { id } });
+  const newStudent = await Student.findByPk(id);
+  res.json(newStudent);
+});
 
 module.exports = router;

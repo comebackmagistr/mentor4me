1621d9de09ff609b52f02f9a7b37789e37d61acf
Sofia Shidakova <fshidakova@mail.ru>
2022-12-05 18:28:37 +0300

    fix bugs

--- client/src/components/Calendar/EditModalCard.jsx
+++ client/src/components/Calendar/EditModalCard.jsx
@@ -3,29 +3,36 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setActive } from '../../redux/modalSlice';
+import { setActiveEdit } from '../../redux/modalSliceEdit';
 import AddEvent from './AddEvent';
+import UpdateEvent from './UpdateEvent';
 
-export default function ModalCard() {
+export default function EditModalCard() {
   const active = useSelector((s) => s.active);
+  const modalContent = useSelector((s) => s.modalContent);
+  console.log((modalContent));
   const dispatch = useDispatch();
   return (
 
     <div className="modal is-active">
-      <div className="modal-background" onClick={() => dispatch(setActive())} />
+      <div className="modal-background" onClick={() => dispatch(setActiveEdit())} />
       <div className="modal-card">
         <header className="modal-card-head">
           <p className="modal-card-title">Modal title</p>
           <button type="button" className="delete" aria-label="close" />
         </header>
         <section className="modal-card-body">
-          <AddEvent />
+          <UpdateEvent modalContent={modalContent} />
         </section>
         <footer className="modal-card-foot">
-          <button type="button" className="button is-success">Save</button>
-          <button type="button" className="button" onClick={() => dispatch(setActive())}>Cancel</button>
+          <button type="button" className="button is-success">Save changes</button>
+          <button type="button" className="button" onClick={() => dispatch(setActiveEdit())}>Cancel</button>
+          <button type="button" className="button">Delete</button>
         </footer>
       </div>
     </div>
 
   );
 }
+
+// "bulma": "^0.9.4",
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,6 +4,8 @@ import searchReducer from './searchSlice';
 import userReducer from './userSlice';
 import eventReducer from './eventSlice';
 import activeReduser from './modalSlice';
+import activeEditReduser from './modalSliceEdit';
+import modelContentReduser from './modelContentSlice';
 
 const store = configureStore({
   reducer: {
@@ -12,6 +14,8 @@ const store = configureStore({
     user: userReducer,
     events: eventReducer,
     active: activeReduser,
+    activeedit: activeEditReduser,
+    modalContent: modelContentReduser,
   },
 });
 export default store;

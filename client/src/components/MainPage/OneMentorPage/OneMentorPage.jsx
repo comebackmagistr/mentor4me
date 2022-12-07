8d4826adb870f8f4533247652913f12c36229d1e
Sesegma <anveegres@icloud.com>
2022-12-07 18:31:12 +0300

    postavili ?

--- client/src/components/MainPage/OneMentorPage/OneMentorPage.jsx
+++ client/src/components/MainPage/OneMentorPage/OneMentorPage.jsx
@@ -39,7 +39,6 @@ export default function OneMentorPage() {
     setOpen(false);
   };
 
-  const navigate = useNavigate();
   /// / ниже логика на отправку отзыва
 
   const [inputReview, setInputReview] = useState({ comment: '', rating: '' });
